f911f1bd900c90ba84cc52a25c47562ed001d59a
Inwerwm <saclainvervm@gmail.com>
2020-04-25 16:41:33 +0900

    プロジェクトの生成

---
+++ SelectVertexOutput/Main.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PEPlugin;
+using PEPlugin.Pmx;
+
+namespace SelectVertexOutput
+{
+    public class SelectVertexOutput : PEPluginClass
+    {
+        public SelectVertexOutput() : base()
+        {
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "プラグイン名";
+            }
+        }
+
+        public override string Version
+        {
+            get
+            {
+                return "0.0";
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "プラグイン説明";
+            }
+        }
+
+        public override IPEPluginOption Option
+        {
+            get
+            {
+                // boot時実行, プラグインメニューへの登録, メニュー登録名
+                return new PEPluginOption(false, true, "プラグイン名");
+            }
+        }
+
+        public override void Run(IPERunArgs args)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+    }
+}
